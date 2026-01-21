b10fe3a65d968dd3a5088747d1bd2b91c8a7743c
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-21 12:06:28 +0530

    commit

---
+++ 3js/js.js
@@ -0,0 +1,57 @@
+//Variable in the Java script 
+//A javascript varible is just a name of starage location
+
+//we can give that name in three ways
+
+//let
+//const
+//var
+
+let username="Mohit sankhyan";
+console.log(username);
+console.log(typeof(username));
+console.log(parseInt(username));
+
+
+let age=26;
+console.log(age);
+console.log(typeof(age));
+
+
+let isHappy=true;
+console.log(isHappy);
+console.log(typeof(isHappy));
+
+let userIntro='Hi, My name is '+username;
+console.log(userIntro);
+console.log(typeof(userIntro));
+
+let a=undefined;
+console.log(a);
+console.log(typeof(undefined));
+
+let x=null;
+console.log(x);
+console.log(typeof(x));
+
+let b;
+console.log(typeof(b));
+
+const data=35;
+console.log(data);
+// console.log(typeof(data));
+
+// data=20;
+// console.log(data); this is give error becaue the const can not change the value again if you you fill the value that is fixxed.
+
+var namedata="mohit sankhyan"
+console.log(namedata);
+
+
+//what is the difference between the let and var
+//let is block scope 
+//var is function scope
+
+//in java script the code is run line by line
+//java script is interperature language not campile language
+// developer of the java scipt is Brendan Eich
