10fa72521256653bf78581acdeaac1a0f42643a3
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-21 12:31:03 +0530

    commit

---
+++ 5js/js.js
@@ -0,0 +1,15 @@
+//Dialog boxes 
+//there are three tupe of  Dialog boxes
+
+//1. alert("your messege goes here")
+//2. confirm("your messege goes here")
+//3. prompt("Your messege goes here")
+
+
+// alert("your messege goes here");
+
+// const isConfirm=confirm("Would you like to proced");
+
+
+let a=prompt('Enter the value of a');
+console.log(a);
