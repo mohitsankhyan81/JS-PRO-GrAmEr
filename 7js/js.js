a6ccf68198fdfd92123fdc77db56fccc6b7c0eaf
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-23 00:48:16 +0530

    commit

---
+++ 7js/js.js
@@ -0,0 +1,51 @@
+//Math in javascript
+
+//mathematical operator +
+//Subtraction operator -
+//multiplaction operator *
+//divison operator /
+//Reminder operator %
+//Ecponentialtion operator **
+
+//Reminder operator
+// let a=11
+// let b=3
+// let ans=a%b
+// console.log(ans);
+
+//Exponentialtion Operator **
+// let a=5
+// let b=3
+// let ans=a**b
+// console.log(ans)
+
+//Math
+// console.log(Math)
+// console.log(Math.PI)
+
+//Squrt
+// console.log(Math.sqrt(144));
+
+//power
+// console.log(Math.pow(2,5));
+
+//Math.floor
+// let a=1.5
+// console.log(Math.floor(a));
+// let b=2.99999
+// console.log(Math.floor(b)); 
+// let c=-1.5
+// console.log(Math.floor(c))  
+
+// Math.round
+// let a=2.4
+// console.log(Math.round(a));
+// let b=-2.3
+// console.log(Math.round(b))
+
+//Math.randam
+// console.log(Math.floor(Math.random())*20);
+
+//n/0 is infinity
+//-n/0 is -infinity
+
