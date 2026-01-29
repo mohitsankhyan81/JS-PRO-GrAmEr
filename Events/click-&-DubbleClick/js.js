aecaace4515679c5f17b467515c4beb84d1f6bf1
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-29 20:52:01 +0530

    event

---
+++ Events/click-&-DubbleClick/js.js
@@ -0,0 +1,11 @@
+let click=document.getElementById("click");
+
+click.onclick=()=>{
+  console.log("Button is clicked");
+}
+
+let hover=document.querySelector(".hover");
+
+hover.onmouseover=()=>{
+  console.log("you are inseted div");
+}
