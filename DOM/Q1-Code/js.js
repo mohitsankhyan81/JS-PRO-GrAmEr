574df2f75f82d2f7f6cdeafebfdc2b0c57bbba2b
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-29 20:05:59 +0530

    createElement style innertext append

---
+++ DOM/Q1-Code/js.js
@@ -0,0 +1,7 @@
+let newbutton=document.createElement('button');
+newbutton.innerText="Click me!";
+
+newbutton.style.background='red';
+newbutton.style.color='white';
+
+document.body.appendChild(newbutton)
