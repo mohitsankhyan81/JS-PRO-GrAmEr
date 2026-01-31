5732b8bbe4603d772356b49b84c371bdf747ce47
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-31 11:15:14 +0530

    advance functioon properties for interview

---
+++ Advance-function/function/js.js
@@ -0,0 +1,27 @@
+//this call apply bind
+//this: class object method
+//strict mode vs mon strict mode
+//golbal object in window it is called window , in node js it is called global
+
+//globalthis points to global object of the environment
+
+// var a=10;
+// b=20;
+
+// console.log(a,b);
+
+//java script bugs
+
+// function greet(name,name){
+//   console.log(name,name);
+// }
+
+// greet("mohit","rohit");
+
+// 'use strict'
+//latest java script by useing strict mode
+
+
+// console.log(global)
+// console.log(globalThis)
+
