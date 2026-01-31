805c839d60df274b2c316c2f9e585a2f6be338fa
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-01-31 12:08:40 +0530

    this keyword use

--- Advance-function/This-keyword/js.js
+++ Advance-function/This-keyword/js.js
@@ -1,3 +1,15 @@
 //learn about this
+//this keyword is global spcope : NodeJs( empty obect ) but in the browser this point out the window 
 
-console.log(this);
+// console.log(this);
+
+const user={
+  name:"Rohit",
+  age:30,
+  greed:function(){
+    console.log(this)
+    console.log(`h1 ${this.name}`);
+  }
+}
+
+user.greed();
