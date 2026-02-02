530aa4c15537cb6dcfe7d78450c501eaf9a48f41
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-02 15:04:38 +0530

    setinterval

---
+++ settimeout-and-setInterval/setinterval/js.js
@@ -0,0 +1,33 @@
+//setinterval 
+// setInterval(() => {
+//   console.log("hello world");
+// }, 1000);
+
+//count from 1 to infinite
+// let count=1;
+// const id=setInterval(() => {
+//   console.log(count);
+//   count++;
+// },1000);
+
+// stop setinterval using clearInterval
+// let count=1;
+// const id=setInterval(() => {
+//   console.log(count);
+//   count++;
+//   if(count===6){
+//     clearInterval(id);
+//   }
+// }, 1000);
+
+
+//user otp bhejne ke baat turant Resent otp click na kare uske liye 30 second ka timer
+
+// let time=30;
+// const id=setInterval(()=>{
+//   console.log(`Resend in ${time}s`);
+//   time--;
+//   if(time<0){
+//     clearInterval(id);
+//   }
+// },1000)
