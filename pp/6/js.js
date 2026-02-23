f41364d82fd8c6533f12f3f269bf28ed0cdcf30b
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-02-23 21:33:16 +0530

    array itarator

---
+++ pp/6/js.js
@@ -0,0 +1,44 @@
+// let arr=[];
+// arr.push(34);
+// arr.push(23);
+// arr.push(97);
+// arr.pop();
+// arr.unshift(11);
+// arr.unshift(98);
+// arr.shift();
+// arr.push(45);
+// arr.push(67);
+// let result=arr.slice(2,4);
+// console.log(result)
+// arr.splice(2,0,57);
+// console.log(arr)
+
+// let arr1=[1,2,3];
+// let arr2=[4,5,6];
+// let merged=arr1.concat(arr2);
+// console.log(merged);
+
+// let arr=[1,2,3,4,5,6];
+// console.log(arr.includes(1));
+// console.log(arr.includes(3));
+
+// let numbers=[5,2,10,1];
+// numbers.sort((a,b)=>a-b);
+// console.log(numbers);
+
+// let arr=[1,2,3,4,5];
+// arr.reverse();
+// console.log(arr);
+
+//for of array ke elements ko directly access karne ke liye use hota hai
+// let arr=[10,20,30,40];
+// for(let vari of arr){
+//     console.log(vari);
+// }
+
+//for in array show the index
+// let arr=[10,20,30,40,50];
+// for(let data in arr){
+//     console.log(data);
+// }
+
