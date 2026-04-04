fe0fd24901e353a2aa3cb33c28d74578d9e5d04c
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-04 18:12:08 +0530

    enum

---
+++ adv-typescript/object Type/src/App.tsx
@@ -0,0 +1,33 @@
+const App = () => {
+  //create a simple object
+  // const car:{type:string,model:string,year:number}={
+  //   type:"Toyota",
+  //   model:"Corolla",
+  //   year:3404
+  // }
+
+  // console.log(car);
+
+  //auto type 
+  // const car={
+  //   type:"toyota"
+  // };
+  // car.type="Mohit"
+  // console.log(car);
+
+  //Optional Properties
+  // const car:{type:string,mileaga:number}={
+  //   type:"toyota"
+  // };
+  // car.mileaga=2000
+  // console.log(car);
+
+  
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
