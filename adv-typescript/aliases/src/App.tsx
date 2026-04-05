27db9ba00f5bc733d3eccdc792b6d22225b1b4fa
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-05 22:25:47 +0530

    interface typescript

---
+++ adv-typescript/aliases/src/App.tsx
@@ -0,0 +1,41 @@
+const App = () => {
+  // type CarYear=number
+  // type CarType=string
+  // type CarModel=string
+  // type Car={
+  //   year:CarYear,
+  //   type:CarType,
+  //   model:CarModel
+  // }
+
+  // const carYear:CarYear=2001
+  // const carType:CarType="Toyota"
+  // const carModel:CarModel="Maruti"
+  // const car:Car={
+  //   year:carYear,
+  //   type:carType,
+  //   model:carModel
+  // }
+  // console.log(car);
+
+  // interface user{
+  //   name:string,
+  //   age:number
+  // }
+  // interface hello extends user{
+  //   color:string
+  // }
+
+  // function data(he:hello){
+  //   return `name: ${he.name} || age: ${he.age} || color: ${he.color}`;
+  // }
+
+  // console.log(data({name:"mohit",age:34,color:"red"}) )
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
